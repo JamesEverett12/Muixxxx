4c484fd380152843fe3769a0d6ea3e080ccbbbd2
JamesEverett12 <61456309+JamesEverett12@users.noreply.github.com>
2025-01-29 17:01:43 +0530

    Update support.js

--- commands/support.js
+++ commands/support.js
@@ -9,10 +9,10 @@ module.exports = {
     options: [],
     run: async (client, interaction, lang) => {
         try {
-            const supportServerLink = "https://discord.gg/xQF9f9yUEM";
-            const githubLink = "https://github.com/GlaceYT";
-            const replitLink = "https://replit.com/@GlaceYT";
-            const youtubeLink = "https://www.youtube.com/@GlaceYT";
+            const supportServerLink = "https://discord.gg/8NhPxUzwWr";
+            const githubLink = "https://github.com/JamesEverett12";
+            const replitLink = "https://replit.com/@JamesEverett12";
+            const youtubeLink = "https://www.youtube.com/@jiro-sasak";
 
             const embed = new EmbedBuilder()
                 .setColor('#b300ff')
